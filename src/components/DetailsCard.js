d809729218f330d37e45f8ed0979e7c56258f983
unknown <wtakkinen@gmail.com>
2023-02-27 21:38:40 +0200

    footer

--- src/components/DetailsCard.js
+++ src/components/DetailsCard.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from "react-router-dom";
 
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,39 @@
-import React from 'react'
+import React from 'react';
+import { useFetch } from '../hooks/useFetch';
+import { AiFillLinkedin, AiFillGithub } from 'react-icons/ai'
 
 const Footer = () => {
+  const { loading, error, data } = useFetch("https://api.github.com/repos/WiljamiT/react-app-aws/commits");
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+  console.log(data)
+
   return (
     <footer>
-        
+      <div className="logos">
+        <AiFillGithub />
+        <p>Wiljami Takkinen | 2023</p>
+        <AiFillLinkedin />
+      </div>
+      <div className="status">
+        <p>{data.slice(0, 1).map((commit) => (
+          <li key={commit.sha}>
+          <p>Site updated and built with commit: <a className="commit" href={commit.html_url}>{commit.sha.slice(0, 7)}</a> at: <i className="date">{new Intl.DateTimeFormat("en-GB", {
+                  timeZone: "Europe/Helsinki",
+                  dateStyle: "medium",
+                  timeStyle: "medium",
+              }).format(new Date(commit.commit.committer.date))}</i>
+              {" "} by: <a href={commit.author.html_url} className="name">{commit.committer.login}</a>
+              
+              </p>
+      </li>
+        ))}</p>
+      </div> 
     </footer>
-    
   )
 }
 
